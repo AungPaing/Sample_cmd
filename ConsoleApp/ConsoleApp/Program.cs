7d2d8aa41206c43aaf25c823782e3ea14fe8f9f4
aungpaing <solarise.cs@gmail.com>
2022-08-30 01:42:09 +0630

    new write line

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("My name is aungpaing.");
+            Console.WriteLine("My favourite language is C#.");
             Console.Read();
         }
     }
